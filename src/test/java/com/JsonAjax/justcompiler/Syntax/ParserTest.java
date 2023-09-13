8fa0c72a38da2f8e0e8dac9ffb032dfb365e8f89
Houssameddine YOUSFI <sw.houssam@gmail.com>
2023-09-13 14:02:41 +0200

    Test unary and binary precedences

--- src/test/java/com/JsonAjax/justcompiler/Syntax/ParserTest.java
+++ src/test/java/com/JsonAjax/justcompiler/Syntax/ParserTest.java
@@ -18,8 +18,8 @@ public class ParserTest {
     @ParameterizedTest
     @MethodSource("getBinaryOperatorPairsStream")
     public void parser_BinaryExpression_HonorsPrecedences(SyntaxKind op1, SyntaxKind op2){
-        int op1precedenced = SyntaxFacts.getBinaryOperatorPrecedence(op1);
-        int op2precedenced = SyntaxFacts.getBinaryOperatorPrecedence(op2);
+        int op1precedence = SyntaxFacts.getBinaryOperatorPrecedence(op1);
+        int op2precedence = SyntaxFacts.getBinaryOperatorPrecedence(op2);
         String op1Text = SyntaxFacts.getText(op1);
         String op2Text = SyntaxFacts.getText(op2);
 
@@ -27,7 +27,7 @@ public class ParserTest {
         Parser parser = new Parser(text);
         SyntaxTree ast = parser.parse();
 
-        if (op1precedenced >= op2precedenced){
+        if (op1precedence >= op2precedence){
             AssertingList e = new AssertingList(ast.getRoot());
             e.AssertNode(SyntaxKind.binaryExpression);
             e.AssertNode(SyntaxKind.binaryExpression);
@@ -57,9 +57,59 @@ public class ParserTest {
 
     }
 
+    @ParameterizedTest
+    @MethodSource("getUnaryBinaryOperatorPairsStream")
+    public void parser_UnaryExpression_HonorsPrecedences(SyntaxKind unaryKind, SyntaxKind binaryKind){
+        int unaryPrecedence = SyntaxFacts.getUnaryOperatorPrecedence(unaryKind);
+        int binaryPrecedence = SyntaxFacts.getBinaryOperatorPrecedence(binaryKind);
+        String unaryText = SyntaxFacts.getText(unaryKind);
+        String binaryText = SyntaxFacts.getText(binaryKind);
+
+        String text = unaryText + " a " + binaryText + " b";
+        Parser parser = new Parser(text);
+        SyntaxTree ast = parser.parse();
+
+        if (unaryPrecedence >= binaryPrecedence){
+            AssertingList e = new AssertingList(ast.getRoot());
+            e.AssertNode(SyntaxKind.binaryExpression);
+            e.AssertNode(SyntaxKind.unaryExpression);
+            e.AssertToken(unaryKind, unaryText);
+            e.AssertNode(SyntaxKind.nameExpression);
+            e.AssertToken(SyntaxKind.identifierToken, "a");
+            e.AssertToken(binaryKind, binaryText);
+            e.AssertNode(SyntaxKind.nameExpression);
+            e.AssertToken(SyntaxKind.identifierToken, "b");
+
+
+
+        }else {
+            AssertingList e = new AssertingList(ast.getRoot());
+            
+            e.AssertNode(SyntaxKind.unaryExpression);
+            e.AssertToken(unaryKind, unaryText);
+            e.AssertNode(SyntaxKind.binaryExpression);   
+            e.AssertNode(SyntaxKind.nameExpression);
+            e.AssertToken(SyntaxKind.identifierToken, "a");
+            e.AssertToken(binaryKind, binaryText);
+            e.AssertNode(SyntaxKind.nameExpression);
+            e.AssertToken(SyntaxKind.identifierToken, "b");
+        }
+
+    }
+
     public static Stream<Arguments> getBinaryOperatorPairsStream(){
         List<Arguments> list = new ArrayList<>();
         for (SyntaxKind kind1 : SyntaxFacts.getBinaryOperatorKinds()) {
+            for (SyntaxKind kind2 : SyntaxFacts.getBinaryOperatorKinds()) {
+                list.add(Arguments.of(kind1,kind2));
+            }
+        }
+        return list.stream();
+    }
+
+    public static Stream<Arguments> getUnaryBinaryOperatorPairsStream(){
+        List<Arguments> list = new ArrayList<>();
+        for (SyntaxKind kind1 : SyntaxFacts.getUnaryOperatorKinds()) {
             for (SyntaxKind kind2 : SyntaxFacts.getBinaryOperatorKinds()) {
                 list.add(Arguments.of(kind1,kind2));
             }
