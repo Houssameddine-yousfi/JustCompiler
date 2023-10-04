448e3c1ce0d2452cd65e9e7fee38fb80e1cf92f5
Houssameddine YOUSFI <sw.houssam@gmail.com>
2023-10-04 16:21:10 +0200

    Add tests for diagnostics

--- src/main/java/com/JsonAjax/justcompiler/Text/TextSpan.java
+++ src/main/java/com/JsonAjax/justcompiler/Text/TextSpan.java
@@ -27,6 +27,42 @@ public class TextSpan {
 
     public static TextSpan fromBounds(int start2, int end) {
         return new TextSpan(start2, end - start2);
-    } 
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + start;
+        result = prime * result + length;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TextSpan other = (TextSpan) obj;
+        if (start != other.start)
+            return false;
+        if (length != other.length)
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "TextSpan [" + start + "..." + length + "]";
+    }
+
+    
+
     
 }
--- src/test/java/com/JsonAjax/justcompiler/EvaluationTest.java
+++ src/test/java/com/JsonAjax/justcompiler/EvaluationTest.java
@@ -2,18 +2,26 @@ package com.JsonAjax.justcompiler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import javax.management.RuntimeErrorException;
+import javax.swing.Spring;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import com.JsonAjax.justcompiler.Syntax.Parser;
 import com.JsonAjax.justcompiler.Syntax.SyntaxTree;
+import com.JsonAjax.justcompiler.Text.AnnotatedText;
+import com.JsonAjax.justcompiler.Text.TextSpan;
 
 public class EvaluationTest {
 
@@ -64,5 +72,109 @@ public class EvaluationTest {
         return list.stream();
     }
 
+    @Test
+    public void Evaluator_VariableDeclaration_Reports_Redeclaration(){
+        String text = """
+            {
+                var x = 10
+                var y = 100
+                {
+                    var x = 10
+                }
+                var [x] = 5
+            }
+        """;
+
+        String diagnostics = "Variable 'x' is already declared.";
+        assertHasDiagnostics(text, diagnostics);
+    }
+
+    @Test
+    public void Evaluator_Name_Reports_Undefined(){
+        String text = """
+            [x] = 10
+        """;
+
+        String diagnostics = "Variable 'x' doesn't exist.";
+        assertHasDiagnostics(text, diagnostics);
+    }
+
+    @Test
+    public void Evaluator_Assignment_Reports_CannotAssign(){
+        String text = """
+            {
+                let x = 5
+                x [=] 10
+            }
+        """;
+
+        String diagnostics = "Variable 'x' is read-only and cannot be assigned to.";
+        assertHasDiagnostics(text, diagnostics);
+    }
+
+    @Test
+    public void Evaluator_Assignment_Reports_CannotConvert(){
+        String text = """
+            {
+                var x = 5
+                x = [true]
+            }
+        """;
+
+        String diagnostics = "Cannot convert type 'class java.lang.Boolean' to type 'class java.lang.Integer'.";
+        assertHasDiagnostics(text, diagnostics);
+    }
+
+    @Test
+    public void Evaluator_Unary_Reports_Undefined(){
+        String text = """
+            [+]true
+        """;
+
+        String diagnostics = "Unary operator '+' is not defined for type 'class java.lang.Boolean'.";
+        assertHasDiagnostics(text, diagnostics);
+    }
+
+    @Test
+    public void Evaluator_Binary_Reports_Undefined(){
+        String text = """
+            10 [+] false
+        """;
+
+        String diagnostics = "Binary operator '+' is not defined for types 'class java.lang.Integer' and 'class java.lang.Boolean'.";
+        assertHasDiagnostics(text, diagnostics);
+    }
+
+    private void assertHasDiagnostics(String text, String diagnosticText){
+        AnnotatedText annotatedText = AnnotatedText.Parse(text);
+        SyntaxTree syntaxTree = SyntaxTree.parse(annotatedText.getText());
+        var compilation = new Compilation(syntaxTree);
+        EvaluationResult result = compilation.evaluate(new HashMap<VariableSymbol,Object>());
+
+        List<String> expectedDiagnostics = AnnotatedText.unindentLines(diagnosticText);
+
+        if(annotatedText.getSpans().size() != expectedDiagnostics.size())
+            throw new RuntimeException( "Test not well definded: Must mark as many spans as the number of expected diagnostics");
+
+        if(annotatedText.getSpans().size() != result.getDiagnostics().size())
+            fail("Number of expected diagnostics does not equal the number of actual diagnostics");
+
+        Iterator<Diagnostic> itr = result.getDiagnostics().iterator();
+        int i =0;
+        while(itr.hasNext()){
+            
+            Diagnostic diag = itr.next();
+
+            String expectedMessage = expectedDiagnostics.get(i);
+            String actualMessage   = diag.getMessage();
+            assertEquals(expectedMessage,actualMessage);
+
+            TextSpan expectedSpan = annotatedText.getSpans().get(i);
+            TextSpan actualSpan   = diag.getSpan();
+            assertEquals(expectedSpan,actualSpan);
+            i++;
+        }
+        
+    }
 
 }
